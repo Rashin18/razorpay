d0f300b509662263824c2ff7ca64ed733fac3cf9
Rashin18 <135828208+Rashin18@users.noreply.github.com>
2025-06-13 12:50:39 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -9,11 +9,10 @@ use App\Http\Controllers\WebhookController;
 });*/
 
 
-Route::middleware(['web', 'auth'])->group(function () {
-    Route::get('/', [PaymentController::class, 'index'])->name('payment');
-    Route::post('/create-order', [PaymentController::class, 'createOrder'])->name('create.order');
-    Route::post('/payment-success', [PaymentController::class, 'paymentSuccess'])->name('payment.success');
-    // routes/web.php
+Route::get('/', [PaymentController::class, 'index']);
+Route::post('/create-order', [PaymentController::class, 'createOrder']);
+Route::post('/payment-success', [PaymentController::class, 'paymentSuccess']);
+Route::get('/my-payments', [PaymentController::class, 'userPayments'])->middleware('auth');
+// routes/web.php
 
-    Route::post('/webhook/razorpay', [WebhookController::class, 'handleWebhook']);
-});
+Route::post('/webhook/razorpay', [WebhookController::class, 'handleWebhook']);
