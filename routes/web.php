7f1a39ca85beb2d2ae0869c06c4a64b6898f0ae6
Rashin18 <135828208+Rashin18@users.noreply.github.com>
2025-06-16 11:48:41 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ use App\Http\Controllers\WebhookController;
 
 Route::get('/', [PaymentController::class, 'index']);
 Route::post('/create-order', [PaymentController::class, 'createOrder']);
-Route::post('/payment-success', [PaymentController::class, 'handlePaymentSuccess']);
+Route::post('/payment-success', [PaymentController::class, 'PaymentSuccess']);
 
 Route::get('/my-payments', [PaymentController::class, 'userPayments'])->middleware('auth');
 // routes/web.php
